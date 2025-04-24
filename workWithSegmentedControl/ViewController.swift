036ec3a3f9c9f142bc0d3141a9b6a4ba3f9c0551
Andrey <Rhymes.un.limit@gmail.com>
2025-04-24 16:56:28 +0300

    add new functionality

--- workWithSegmentedControl/ViewController.swift
+++ workWithSegmentedControl/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
         
     }
 
 
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The first segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The second segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The third segment is selected"
+            label.textColor = .yellow
+        default:
+            label.text = "Something wrong"
+        }
     }
 }
 
