36dd7d27e0407e80ca1062d6135bec14bf3cf84f
Andrey <Rhymes.un.limit@gmail.com>
2025-04-24 16:49:14 +0300

    add element

--- workWithSegmentedControl/ViewController.swift
+++ workWithSegmentedControl/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+    }
 }
 
